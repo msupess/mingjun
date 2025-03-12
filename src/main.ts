beaa33e33e98ebfa6a9fe718aed6d6ec2c51e5ce
chenming <c1026287787@163.com>
2025-03-12 15:16:29 +0800

    feat: 增加一些

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import App from "./App.vue";
 import './style.scss'
 import naive from 'naive-ui'
 
+// 腾讯部署网站：https://www.tencentcloud.com/
+
 export function createApp() {
   const app = createSSRApp(App);
   app.use(naive)
